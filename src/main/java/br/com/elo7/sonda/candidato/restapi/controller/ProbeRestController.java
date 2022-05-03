fa1acf076181cb01dc0f22a02e48655ca4161c4d
atavares <allan.tavares@msn.com>
2022-05-02 21:58:54 -0300

    Adiciona log no Controller

--- src/main/java/br/com/elo7/sonda/candidato/restapi/controller/ProbeRestController.java
+++ src/main/java/br/com/elo7/sonda/candidato/restapi/controller/ProbeRestController.java
@@ -3,6 +3,8 @@ package br.com.elo7.sonda.candidato.restapi.controller;
 import java.net.URI;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -15,15 +17,19 @@ import br.com.elo7.sonda.candidato.model.Probe;
 import br.com.elo7.sonda.candidato.service.ProbeService;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
+
 @Controller
 @RequestMapping("/planet-with-probes")
 public class ProbeRestController {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(ProbeRestController.class);
 	@Autowired
 	private ProbeService probeService;
 
 	@PostMapping
-    public ResponseEntity<List<Probe>> register(@RequestBody InputForm inputForm) {
-
+    public ResponseEntity<List<Probe>> register(@RequestBody @Valid InputForm inputForm) {
+		LOGGER.info("register planet with probe to width {} height {} probes {}", inputForm.getWidth(), inputForm.getHeight(), inputForm.getProbes());
 		return ResponseEntity.created(ServletUriComponentsBuilder.fromCurrentRequestUri()
 																 .build()
 				                                                 .toUri())
