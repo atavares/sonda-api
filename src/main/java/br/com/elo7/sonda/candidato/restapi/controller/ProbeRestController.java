2fbe3ea01f91db17856f793260016516de5389c3
atavares <allan.tavares@msn.com>
2022-05-05 10:19:21 -0300

    adicionado documentação da Probe API

--- src/main/java/br/com/elo7/sonda/candidato/restapi/controller/ProbeRestController.java
+++ src/main/java/br/com/elo7/sonda/candidato/restapi/controller/ProbeRestController.java
@@ -4,14 +4,18 @@ import java.net.URI;
 import java.util.List;
 
 import br.com.elo7.sonda.candidato.restapi.controller.dto.ProbeDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import br.com.elo7.sonda.candidato.restapi.controller.form.InputForm;
 import br.com.elo7.sonda.candidato.model.Probe;
@@ -20,7 +24,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
 
-@Controller
+@RestController
 @RequestMapping("/planet-with-probes")
 public class ProbeRestController {
 
@@ -28,6 +32,12 @@ public class ProbeRestController {
 	@Autowired
 	private ProbeService probeService;
 
+	@Operation(summary = "Launch probe in planet")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "201", description = "Probe hurled",
+					content = { @Content(mediaType = "application/json") }),
+			@ApiResponse(responseCode = "400", description = "Invalid parameter sent",
+					content = @Content)})
 	@PostMapping
     public ResponseEntity<List<ProbeDTO>> register(@RequestBody @Valid InputForm inputForm) {
 		LOGGER.info("register planet with probe to width {} height {} probes {}", inputForm.getWidth(), inputForm.getHeight(), inputForm.getProbes());
